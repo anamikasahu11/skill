321e9c6ca95684539bfd8e78d84bd0186a52e38a
anamikasahu11 <anamikasahu.pune@gmail.com>
2025-09-30 10:23:21 +0530

    Update LCA 2.js

--- LCA 2.js
+++ LCA 2.js
@@ -1,3 +1,4 @@
+//Write a program  to print numbers from 1 to 30 but skip the multiples of 3(using continue)
 for (let i = 1; i <= 30; i++) {
     if (i % 3 === 0) {
         continue;
@@ -5,6 +6,7 @@ for (let i = 1; i <= 30; i++) {
     console.log(i);
 }
 
+//Write a program to display numbers from 10 to 50 but skip the numbers that are divisible by 7(using continue)
 for (let i = 10; i <= 50; i++) {
     if (i % 7 === 0) {
         continue; 
