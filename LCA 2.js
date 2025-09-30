fa3d97b6826f12ecc058b53a56e52c152b4f7573
anamikasahu11 <anamikasahu.pune@gmail.com>
2025-09-30 10:13:30 +0530

    LCA 2.js

---
+++ LCA 2.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 30; i++) {
+    if (i % 3 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+for (let i = 10; i <= 50; i++) {
+    if (i % 7 === 0) {
+        continue; 
+    }
+    console.log(i);
+}
